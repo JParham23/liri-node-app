2ea83acacfd94e1af8cedeaf6ebdfeb9f6d7e750
Jamar Parham <Jamar720@gmail.com>
2018-04-14 04:52:05 -0400

    files added

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key:'eY5f4kIXDrCmiPggEvImDLohB',
+  consumer_secret:'rkz9LZORz1NqZrKd8UBSllumxROZG1WSb21PscAHxFSQZTbgvG',
+  access_token_key:'985044918619004928-UMXKBNlKJ2WFIcktU2F5Ox3WcD4tMR4',
+  access_token_secret:'EWdk743R3WBQnkeUiSwp42InkWTaw0mih6QuyY2o29eq0',
+};
+
+exports.spotifyKeys = {
+  id:'9ee1822dd75b4dc68f8264f79fafcc3e',
+  secret:'c4279590b3fd414a94747f95499c85a6' 
+};
+
+module.exports = { twitterKeys, spotifyKeys };
